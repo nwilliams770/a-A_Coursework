52028a5b309944a8b3f11333b325a56486e3e8de
Nicholas Williams <nwilliams770@gmail.com>
2017-10-05 17:02:59 -0700

    init commit

---
+++ tile.rb
@@ -0,0 +1,33 @@
+class Tile
+  def initialize(value, visibility = false)
+    @visiblity = visbility
+    @value = value
+
+  end
+
+  def is_bomb?
+    @value == "b"
+  end
+
+  def is_empty?
+    @value == 0
+  end
+
+  def to_s
+    if @visibility
+      if is_bomb?
+        "b"
+      elsif is_empty?
+        "0"
+      else
+        @value.to_s
+      end
+    else
+      "_"
+    end
+  end
+
+  def reveal
+    @visiblity = true
+  end
+end
