c54d19fcc071e10fed27d4bdd769dce9b497a6a8
Nicholas Williams <nwilliams770@gmail.com>
2017-10-13 14:37:23 -0700

    Created HashMap

--- my_hash_map/lib/p01_int_set.rb
+++ my_hash_map/lib/p01_int_set.rb
@@ -1,19 +1,29 @@
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max) {false}
   end
 
   def insert(num)
+    if is_valid?(num)
+      @store[num] = true
+    else
+      raise "Out of bounds"
+    end
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    return false if num < 0
+    num < @store.count
   end
 
   def validate!(num)
@@ -27,18 +37,26 @@ class IntSet
   end
 
   def insert(num)
+    self[num]<<num
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+    else
+      raise "Number not in set"
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -55,18 +73,32 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if !(self[num].include?(num))
+      if @count >= num_buckets
+        resize!
+        self[num] << num
+      else
+        self[num] << num
+      end
+      @count += 1
+    end
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +106,16 @@ class ResizingIntSet
   end
 
   def resize!
+      # add same amt of buckets
+    # (num_buckets*2).times do
+    #   @store << Array.new
+    # end
+    #reassign els into new buckets e.g delete and re-insert
+    set = @store
+    @store = Array.new(num_buckets*2) { Array.new }
+    set = set.flatten.compact
+    set.each do |el|
+      @store[el % num_buckets] << el
+    end
   end
 end
