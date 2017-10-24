f5b37019b429dc05dae5e40be284730a16b05927
Nicholas <nwilliams770@gmail.com>
2017-10-23 18:16:40 -0700

    Create routers and controllers

---
+++ app/models/artwork_share.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: artwork_shares
+#
+#  id         :integer          not null, primary key
+#  artwork_id :integer          not null
+#  viewer_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class ArtworkShare < ApplicationRecord
+  validates :artwork_id, uniqueness: { scope: :viewer_id,
+  message: "can only share artwork with viewer once" }
+
+  belongs_to :artwork,
+    primary_key: :id,
+    foreign_key: :artwork_id,
+    class_name: 'Artwork'
+
+  belongs_to :viewer,
+    primary_key: :id,
+    foreign_key: :viewer_id,
+    class_name: 'User'
+
+
+
+end
